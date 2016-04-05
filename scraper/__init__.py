d3068785d6f92f18f93f41f8f9eaf50783e1aea6
bpeterso2000 <bpeterso2000@yahoo.com>
2016-04-04 22:02:16 -0400

    added relative imports for remaining modules

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -6,6 +6,10 @@ A simple recursive web scraper
 :license: Apache 2.0, see LICENSE for more details.
 """
 
+from . import cli
+from . import exceptions
 from . import htmlpage
+from . import htmltidy
+from . import loadjson
 from . import verbosescraper
 from . import webscraper
