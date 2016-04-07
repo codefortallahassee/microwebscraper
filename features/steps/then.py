38e27de6d64e5ad96ff02b88ef4cafdde9271f82
bpeterso2000 <bpeterso2000@yahoo.com>
2016-04-07 12:17:44 -0400

    added diff dump for dict & readlines when assertions fail

--- features/steps/then.py
+++ features/steps/then.py
@@ -1,3 +1,4 @@
+import difflib
 import json
 import os.path
 
@@ -5,6 +6,39 @@ from behave import then
 from nose.tools import assert_in, eq_
 
 current_dir = os.path.dirname(__file__)
+
+
+class DictDiffer(object):
+    """
+    Calculate the difference between two dictionaries as:
+    (1) items added
+    (2) items removed
+    (3) keys same in both but changed values
+    (4) keys same in both and unchanged values
+
+    reference: https://github.com/hughdbrown/dictdiffer
+    """
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.current_keys, self.past_keys = [
+            set(d.keys()) for d in (current_dict, past_dict)
+        ]
+        self.intersect = self.current_keys.intersection(self.past_keys)
+
+    def added(self):
+        return self.current_keys - self.intersect
+
+    def removed(self):
+        return self.past_keys - self.intersect
+
+    def changed(self):
+        return set(o for o in self.intersect
+                   if self.past_dict[o] != self.current_dict[o])
+
+    def __str__(self):
+        print('\n'.join('Added:   ' + self.added(),
+                        'Removed: ' + self.removed(),
+                        'Changed: ' + self.changed()))
 
 
 @then('display an informative file not found message')
@@ -20,13 +54,26 @@ def step_match_output(context, output):
 
 @then('the output should match the file "{file_}"')
 def step_output_should_match_file(context, file_):
-    output = open(os.path.join(current_dir, file_))
-    if file_.endswith('.json'):
-        context.output = json.loads(context.output)
-        output = json.load(output)
-    else:
-        output = output.read()
-    eq_(context.output, output)
+    with open(os.path.join(current_dir, file_)) as output:
+        if file_.endswith('.json'):
+            context.output = json.loads(context.output)
+            output = json.load(output)
+            try:
+                eq_(context.output, output)
+            except AssertionError:
+                print(str(DictDiffer(context.output, output)))
+                raise
+
+        else:
+            context = list(map(str.strip, context.output.splitlines()))
+            expected_result = list(map(str.strip, output.readlines()))
+            try:
+                eq_(context, expected_result)
+            except AssertionError:
+                d = difflib.Differ()
+                for line in d.compare(context, expected_result):
+                    print(line)
+                raise
 
 
 @then('there should be no output')
