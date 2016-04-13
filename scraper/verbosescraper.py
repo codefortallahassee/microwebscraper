6fa2826ce330392d49fd62a2a1c47992a47c5358
bpeterso2000 <bpeterso2000@yahoo.com>
2016-04-12 22:21:07 -0400

    removed keycolor option & updated verbose behave feature output

--- scraper/verbosescraper.py
+++ scraper/verbosescraper.py
@@ -1,76 +1,61 @@
 import click
 
-from .webscraper import do_xpath, xpath_returns_text
+from webscraper import do_xpath, xpath_returns_text
 
-LABEL_COLOR = 'green'
-SEPARATOR_CHAR = '-'
-SEPARATOR_SIZE = 76
-SEPARATOR_COLOR = 'yellow'
-DEFAULT_SEPARATOR = click.style(SEPARATOR_CHAR * SEPARATOR_SIZE,
-                                fg=SEPARATOR_COLOR)
+
+def color_alert(label, color='red', bold=True):
+    return click.style(repr(label), fg=color, bold=bold)
+
+
+def color_label(label, color='green'):
+    return click.style(label, color)
+
+
+def color_separator(sepchar='-', sepsize=76, sepcolor='yellow'):
+    return click.style(sepchar * sepsize, fg=sepcolor)
 
 
 def show_keys(keys):
     return ''.join(map('[{!r}]'.format, keys))
 
 
-def text(label):
-    return label
-
-
-def color_label(label, color=LABEL_COLOR):
-    return click.style(label, color)
-
-
-def alert(label, color='red', bold=True):
-    return click.style(label, fg=color, bold=bold)
-
-
-def color_separator(sepchar=SEPARATOR_CHAR, sepsize=SEPARATOR_SIZE,
-                    sepcolor=SEPARATOR_COLOR):
-    return click.style(sepchar * sepsize, fg=sepcolor)
-
-
-def first(seq):
+def get_first_element(nodes):
     try:
-        return seq[0]
+        return nodes[0]
     except IndexError:
-        return repr(seq)
+        return repr(nodes)
 
 
-def get_tag(seq):
-    el = first(seq)
-    return el if isinstance(el, str) else '<{}>'.format(seq[0].tag)
+def get_tag(elements):
+    first = get_first_element(elements)
+    return first if isinstance(first, str) else '<{}>'.format(elements[0].tag)
 
 
-def verbose_scrape(etree, config, sep='', label=text, keys=None, xpath=None):
-    # import pdb; pdb.set_trace()
-
+def verbose_scrape(etree, config, sep=color_separator, label=color_label,
+                   alert=color_alert, keys=None, xpath=None):
     if keys is None:
         keys, xpath = [], []
     yield label('Page content length: ') + str(len(etree.text_content()))
     for key, value in config.items():
         if isinstance(value, str) and xpath_returns_text(value):
-            result = first(do_xpath(value, etree))
-            if not result:
-                result = alert(repr(result))
+            result = get_first_element(do_xpath(value, etree))
             yield ''
             yield label('  key:   ') + show_keys(keys + [key])
             yield label('  xpath: ') + '/'.join(xpath + [value])
-            yield label('  value: ') + result
+            yield label('  value: ') + result if result else alert(result)
         else:
             keys.append(key)
             xpath.append(value[0])
-            nodes = do_xpath(value[0], etree)
-            tag = get_tag(nodes)
+            elements = do_xpath(value[0], etree)
+            tag = get_tag(elements)
             yield sep
             yield label('key:      ') + show_keys(keys)
             yield label('xpath:    ') + '/'.join(xpath)
-            yield label('elements: ') + '{} * {}'.format(tag, len(nodes))
+            yield label('elements: ') + '{} * {}'.format(tag, len(elements))
             yield sep
-            for n, node in enumerate(nodes):
+            for n, node in enumerate(elements):
                 fmt_xpath = ['({})[{}]'.format('/'.join(xpath), n + 1)]
-                for step in verbose_scrape(node, value[1], sep, label,
+                for step in verbose_scrape(node, value[1], sep, label, alert,
                                            keys + [n], fmt_xpath):
                     yield step
                 yield sep
